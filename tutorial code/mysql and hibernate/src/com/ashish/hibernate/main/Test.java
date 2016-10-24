99e9e563da4c273a925e66731b32c40058731358
Ashishkumar Gupta <adoreashish@gmail.com>
2016-10-24 20:46:36 +0530

    Added mysql and hibernate configuration and a sample demo

---
+++ tutorial code/mysql and hibernate/src/com/ashish/hibernate/main/Test.java
@@ -0,0 +1,11 @@
+package com.ashish.hibernate.main;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println(System.getProperty("java.home"));
+		System.out.println("Ashish");
+	}
+
+}
